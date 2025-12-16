d05946f50a4f5025a5677083136fbeb922d0329e
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-12-16 16:55:20 +0400

    remove cli

--- src/rhiza/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Utility tools and command-line helpers for the Rhiza project.
-
-This package groups small, user-facing utilities that can be invoked from
-the command line or other automation scripts.
-"""
--- src/rhiza/commands/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Command implementations for the Rhiza CLI.
-
-This package contains the functions that back Typer commands exposed by
-`rhiza.cli`, such as `hello` and `inject`.
-"""
