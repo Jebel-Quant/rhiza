25fda0a453601773dc677251bc40b5b1c61165b1
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-12-22 13:38:35 +0400

    careful if benchmark.json does not exist

--- tests/test_rhiza/benchmarks/analyze_benchmarks.py
+++ tests/test_rhiza/benchmarks/analyze_benchmarks.py
@@ -21,6 +21,11 @@ from pathlib import Path
 import pandas as pd
 import plotly.express as px
 
+# check if the file exists at all
+if not Path(__file__).parent.joinpath("benchmarks.json").exists():
+    logging.warning("benchmarks.json not found; skipping analysis and exiting successfully.")
+    sys.exit(0)
+
 # Load pytest-benchmark JSON
 with open(Path(__file__).parent / "benchmarks.json") as f:
     # Do not continue if JSON is invalid (e.g. empty file)
