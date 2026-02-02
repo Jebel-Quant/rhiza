88b93b005ee88acf1c700e1c6e641d73cbd207da
HarryCampion <harrycampion21@gmail.com>
2026-02-02 22:05:35 +0000

    add test-all

--- .rhiza/rhiza.mk
+++ .rhiza/rhiza.mk
@@ -294,6 +294,10 @@ help: print-logo ## Display this help message
 version-matrix: install-uv ## Emit the list of supported Python versions from pyproject.toml
 	@${UV_BIN} run .rhiza/utils/version_matrix.py
 
+test-all: install ## run all tests, typecheck, security scans, mutation testing, and rhiza validation
+	@$(MAKE) test
+	@$(MAKE) validate
+
 print-% : ## print the value of a variable (usage: make print-VARIABLE)
 	@printf "${BLUE}[INFO] Printing value of variable '$*':${RESET}\n"
 	@printf "${BOLD}Value of $*:${RESET}\n"
